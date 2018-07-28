08c9f369ba21697fe1ec6c65a8497ef1151f7cee
Yevhen Kuraiev <yevhen.kuraiev@gmail.com>
2018-07-28 07:12:26 +0300

    bug fixing

--- Answers/Answers/Views/QuestionsPage.xaml.cs
+++ Answers/Answers/Views/QuestionsPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Linq;
+using Answers.Models;
 
 namespace Answers.Views
 {
@@ -9,6 +13,20 @@ namespace Answers.Views
 		public QuestionsPage ()
 		{
             InitializeComponent();
-		}
+        }
+
+	    private void ListView_OnItemAppearing(object sender, ItemVisibilityEventArgs e)
+	    {
+	        var items = ListView.ItemsSource as ObservableCollection<QuestionModel>;
+	        var currentIdx = items?.IndexOf((QuestionModel) e.Item);
+	        ImageArrow.IsVisible = currentIdx > 6;
+	    }
+
+	    private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+	    {
+	        var items = ListView.ItemsSource as ObservableCollection<QuestionModel>;
+	        ListView.ScrollTo(items?.First(), ScrollToPosition.End, true);
+	    }
+
 	}
 }
