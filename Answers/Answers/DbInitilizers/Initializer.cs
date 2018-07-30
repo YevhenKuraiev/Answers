583fa5de39deb5e17232fdce2a37eb551b55dca3
Yevhen Kuraiev <yevhen.kuraiev@gmail.com>
2018-07-30 22:23:11 +0300

    Add questions

--- Answers/Answers/DbInitilizers/Initializer.cs
+++ Answers/Answers/DbInitilizers/Initializer.cs
@@ -16,7 +16,6 @@ namespace Answers.DbInitilizers
             list.Add(new QuestionModel("6. The bee was buzzing among ____ flowers in the vase.", "D) the"));
             list.Add(new QuestionModel("7. He turned and saw _____ crying boy.", "D) the"));
             list.Add(new QuestionModel("8. What _____ marvelous English souvenirs you’ve bought!", "А) —"));
-
             list.Add(new QuestionModel("9. We always have ____ dinner at twelve o'clock", "А) —"));
             list.Add(new QuestionModel("10. My mother was _____ only person whom I told waht had happened", "А) —"));
             list.Add(new QuestionModel("11. ___ man under such circunstances is always very helpless", "C) A"));
@@ -42,6 +41,77 @@ namespace Answers.DbInitilizers
             list.Add(new QuestionModel("31. Michael has got ____ English books at home", "C) a lot of"));
             list.Add(new QuestionModel("32. This suitcase is _____ than that one", "A) heavier"));
             list.Add(new QuestionModel("33. When Ted found a new job, he became ____ on his parents", "C) less dependent"));
+
+
+            list.Add(new QuestionModel("68. What happened to the old mail carrier? He ____ to a new neighborhood to work last week", "D) sent"));
+            list.Add(new QuestionModel("69. Gold ____ in California in the 19 century", "А) was discovered"));
+            list.Add(new QuestionModel("70. What beautiful dress you're wearing! Thanks! It ____ especially for me by a French tailor", "D) was being made"));
+            list.Add(new QuestionModel("71. Who does the washing-up in your house? - The dishes __ by my brother and then they __ by my sister", "A) are washed, are dried"));
+            list.Add(new QuestionModel("72. Why can't I use your car? Because it ____ at the moment", "D) is being serviced"));
+            list.Add(new QuestionModel("73. When I realized I was wrong. I apologized ____ my mistake", "A) for"));
+            list.Add(new QuestionModel("74. Who is going to look ____ your children while you are away", "D) after"));
+            list.Add(new QuestionModel("75. What are you accused ____?", "C) for"));
+            list.Add(new QuestionModel("76. Here is the man who insists ____ your coming", "D) on"));
+            list.Add(new QuestionModel("77. Nobody could account ____ his objection to our plan", "C) for"));
+            list.Add(new QuestionModel("78. Her son succeeded ___ winning the prize in the competition", "C) in"));
+            list.Add(new QuestionModel("79. You ____ stop when the traffic light is red", "D) must"));
+            list.Add(new QuestionModel("80. Sorry, I ___ go now. My bus leaves in a few minutes", "B) have to"));
+            list.Add(new QuestionModel("81. He ____ get a taxi because I gave him a lift", "B) didn't need to"));
+            list.Add(new QuestionModel("82. This is the new catalogue ___ I have it for a moment, please?", "B) Can"));
+            list.Add(new QuestionModel("83. He ____ have left the keys on the table", "D) might"));
+            list.Add(new QuestionModel("84. ____ please give me a call", "C) He should come"));
+            list.Add(new QuestionModel("85. I ____ stay at home as I felt very bad", "D) hat to"));
+            list.Add(new QuestionModel("86. He was trying to repair his car. He seemed to ____ repair it as it is working", "B) be able to"));
+            list.Add(new QuestionModel("87. You ____ go to the college immediatly. Do you understand me?", "D) must"));
+            list.Add(new QuestionModel("88. If I ____ some spare time, I would have devoted it to reading", "C) had had"));
+            list.Add(new QuestionModel("89. If I see Tim. I ____ him his book.", "B) will give"));
+            list.Add(new QuestionModel("90. You ____ better results if you apply this method", "A) will achieve"));
+            list.Add(new QuestionModel("91. They denied ____ the money", "C) stealing"));
+            list.Add(new QuestionModel("92. I don't enjoy ____ very much.", "C) driving"));
+            list.Add(new QuestionModel("93. I don't want ___ out tonight I'm too tired", "B) to go"));
+            list.Add(new QuestionModel("94. Why do you keep ___ me questions? Can't you leave me alone?", "D) asking"));
+            list.Add(new QuestionModel("95. The boy gave up ___ to escape from the lessons", "A) trying"));
+            list.Add(new QuestionModel("96. Has it stopped ___ yet?", "A) raining"));
+            list.Add(new QuestionModel("97. Can you remind me ___ some coffee when we go out?", "B) to buy"));
+            list.Add(new QuestionModel("98. Did you advise him ___ to the police? - No, I didn't like ___ any advice on such a dificult maner.", "С) to go, to give"));
+            list.Add(new QuestionModel("99. It's no use ___ to him", "B) to write"));
+            list.Add(new QuestionModel("100. I wish it ___ raining. I have to go to the shop", "C) has stopped"));
+
+
+            //variant 2
+            list.Add(new QuestionModel("1. He found ___ purse in the street yesterday", "B) a"));
+            list.Add(new QuestionModel("2. I saw him ___ day before yesterday", "B) a"));
+            list.Add(new QuestionModel("3. Your father is such ____ interesting person", "C) an"));
+            list.Add(new QuestionModel("4. She had ___ most intelligent brown eyes Tim had ever seen", "D) the"));
+            list.Add(new QuestionModel("5. She turned and saw ____ laughing girl", "D) the"));
+            list.Add(new QuestionModel("6. ___ British are a nation of newspaper readers", "D) The"));
+            list.Add(new QuestionModel("7. What ____ beautiful picture you've bought", "B) a"));
+            list.Add(new QuestionModel("8. ____ Times is the most popular newspaper among British readers", "A) -"));
+            list.Add(new QuestionModel("9. I asked my father for ____ advice about finding ____ job", "C) an, the"));
+            list.Add(new QuestionModel("10. My friends have never travelled across ____ Atlantic", "B) the"));
+            list.Add(new QuestionModel("11. ___ Manhattan is in ____ New York", "C) -, -"));
+            list.Add(new QuestionModel("12. My ___ room is very comfortable", "А) father's and mother's"));
+            list.Add(new QuestionModel("13. Jane usually goes to college ____ bus", "А) by"));
+            list.Add(new QuestionModel("14. Don't worry! Everything is ___ control", "D) under"));
+            list.Add(new QuestionModel("15. That is what he wanted to begin ____", "B) with"));
+            list.Add(new QuestionModel("16. I've been here ____ two weeks", "C) for"));
+            list.Add(new QuestionModel("17. Ann came ____ the room and sat down ___ the table", "A) into, at"));
+            list.Add(new QuestionModel("18. My friend rang me up and asked me out ___ a walk", "C) for"));
+            list.Add(new QuestionModel("19. I'am hungry. I want ___ to eat.", "B) something"));
+            list.Add(new QuestionModel("20. We had to walk home because there was ___ bus", "D) no"));
+            list.Add(new QuestionModel("21. How long have you and Bill known each ___?", "A) other"));
+            list.Add(new QuestionModel("22. We've got ___ time before the train leaves. Let's go and have a drink", "D) a little"));
+            list.Add(new QuestionModel("23. She is lucky. She doen't have ____ problems", "A) many"));
+            list.Add(new QuestionModel("24. The children hurt ____ when they fell over", "C) themselves"));
+            list.Add(new QuestionModel("25. He wants more milk. Give him ___", "C) some"));
+            list.Add(new QuestionModel("26. I saw ____ at the window", "A) somebody"));
+            list.Add(new QuestionModel("27. We have done it ____", "C) ourselves"));
+            list.Add(new QuestionModel("28. Could you give me a pen. I left ___ at home.", "C) my"));
+            list.Add(new QuestionModel("29. ____ of the students knows English?", "B) Which"));
+            list.Add(new QuestionModel("30. There was ____ to do but wait", "B) evetything"));
+            list.Add(new QuestionModel("31. We'll have to leave a bit ____ than we planned", "B) earlier"));
+            list.Add(new QuestionModel("32. What is ____ river in the world?", "D) the longest"));
+            list.Add(new QuestionModel("33. February was ____ month of the year", "B) the coldest"));
             return list;
         }
 
